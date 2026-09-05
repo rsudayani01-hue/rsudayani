9a0cf705f4278bd642350b1b465a146addb19ae9
Admin <admin@email.com>
2026-09-05 22:59:50 +0700

    fix: Remove deprecated swcMinify from next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Disable SWC minify to reduce memory during build
-  swcMinify: false,
-  // Reduce workers to save memory
-  experimental: {
-    forceSwcTransforms: false,
-  },
 };
 
 module.exports = nextConfig;
